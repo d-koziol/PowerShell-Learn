b1ca378b2cbf2312fbfcbd247084b2ff07d5aa93
Damian Koziol <d.p.koziol@gmail.com>
2025-03-06 12:08:04 +0100

    Write-log function added. Timestamps added.

--- Backup_Script_v1/backup_script_v1.ps1
+++ Backup_Script_v1/backup_script_v1.ps1
@@ -3,18 +3,27 @@ $sourcePath = "C:\Users\$($env:USERNAME)\Documents\"
 $destinationPath = "C:\Users\$($env:USERNAME)\Backup\"
 $logFile = "C:\Users\$($env:USERNAME)\Backup\backup_log.log"
 
+function Write-log {
+    param (
+        [string]$message
+    )
+    $timeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logMessage = "$timeStamp - $message"
+    Write-Output $logMessage | Out-File $logFile -Append
+}
+
 #Script checks Backup folder and creates it if it doesn't exists.
 if (!(Test-Path $destinationPath )) {
     New-Item $destinationPath -ItemType Directory
-    Write-Output "'Backup' folder has been created succesfully." | Out-File $logFile -Append
+    Write-log "'Backup' folder has been created successfully." 
 } else {
-    Write-Output "'Backup'folder already exists." | Out-File $logFile -Append
+    Write-log "'Backup' folder already exists." 
 }
 
 #Script checks Log file and creates it if it doesn't exists.
 if (!(Test-Path $logFile)) {
     New-Item $logFile -ItemType File
-    Write-Output "'Log' file was created." | Out-File $logFile -Append
+    Write-log "'Log' file was created successfully." 
 } else {
-    Write-Output "'Log' file already exists." | Out-File $logFile -Append
+    Write-log "'Log' file already exists." 
 }
