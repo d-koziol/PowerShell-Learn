31c7cac75d2bfe43a456d15c13817aba79c688f3
Damian Koziol <d.p.koziol@gmail.com>
2025-03-06 11:24:48 +0100

    Log file creating loop added.

--- Backup_Script_v1/backup_script_v1.ps1
+++ Backup_Script_v1/backup_script_v1.ps1
@@ -1,4 +1,20 @@
 $userName = $env:USERNAME
-$sourcePath = C:\Users\$($env:USERNAME)\Documents\
-$destinationPath = C:\Users\$($env:USERNAME)\Backup\
-$logFile = C:\Users\$($env:USERNAME)\
+$sourcePath = "C:\Users\$($env:USERNAME)\Documents\"
+$destinationPath = "C:\Users\$($env:USERNAME)\Backup\"
+$logFile = "C:\Users\$($env:USERNAME)\Backup\backup_log.log"
+
+#Script checks Backup folder and creates it if it doesn't exists.
+if (!(Test-Path $destinationPath )) {
+    New-Item $destinationPath -ItemType Directory
+    Write-Output "'Backup' folder has been created succesfully." | Out-File $logFile -Append
+} else {
+    Write-Output "'Backup'folder already exists." | Out-File $logFile -Append
+}
+
+#Script checks Log file and creates it if it doesn't exists.
+if (!(Test-Path $logFile)) {
+    New-Item $logFile -ItemType File
+    Write-Output "'Log' file was created." | Out-File $logFile -Append
+} else {
+    Write-Output "'Log' file already exists." | Out-File $logFile -Append
+}
