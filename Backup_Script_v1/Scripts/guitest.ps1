fdb3bc07be4c99ade95ef3f6196e6e1207c94e18
Damian Koziol <d.p.koziol@gmail.com>
2025-03-07 14:11:06 +0100

    Enable Button Added

--- Backup_Script_v1/Scripts/guitest.ps1
+++ Backup_Script_v1/Scripts/guitest.ps1
@@ -2,3 +2,12 @@ $form = New-Object System.Windows.Forms.Form
 $form.Text = "Backup Settings"
 $form.Size = New-Object System.Drawing.Size(300,200)
 $form.StartPosition = "CenterScreen"
+
+$enableBtn = New-Object System.Windows.Forms.Button
+$enableBtn.Location = New-Object System.Drawing.Point(75,120)
+$enableBtn.Size = New-Object System.Drawing.Size(75,23)
+$enableBtn.Text = "Enable"
+$enableBtn.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $enableBtn
+$form.Controls.Add($enableBtn)
+
