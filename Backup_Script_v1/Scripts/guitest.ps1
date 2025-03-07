ee4fa42ae2e441032c7b322308fdd04031a24e22
Damian Koziol <d.p.koziol@gmail.com>
2025-03-07 14:37:49 +0100

    Buttons and checkboxes edited

--- Backup_Script_v1/Scripts/guitest.ps1
+++ Backup_Script_v1/Scripts/guitest.ps1
@@ -3,30 +3,26 @@ $form.Text = "Backup Settings"
 $form.Size = New-Object System.Drawing.Size(300,200)
 $form.StartPosition = "CenterScreen"
 
-$enableBtn = New-Object System.Windows.Forms.Button
-$enableBtn.Location = New-Object System.Drawing.Point(75,120)
-$enableBtn.Size = New-Object System.Drawing.Size(75,23)
-$enableBtn.Text = "Enable"
-$enableBtn.DialogResult = [System.Windows.Forms.DialogResult]::OK
-$form.AcceptButton = $enableBtn
-$form.Controls.Add($enableBtn)
-
-$disableBtn = New-Object System.Windows.Forms.Button
-$disableBtn.Location = New-Object System.Drawing.Point(150,200)
-$disableBtn.Size = New-Object System.Drawing.Size(75,23)
-$disableBtn.Text = "Disable"
-$disableBtn.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
-$form.CancelButton = $disableBtn
-$form.Controls.Add($disableBtn)
+$acceptBtn = New-Object System.Windows.Forms.Button
+$acceptBtn.Location = New-Object System.Drawing.Point(100,120)
+$acceptBtn.Size = New-Object System.Drawing.Size(75,23)
+$acceptBtn.Text = "Accept"
+$acceptBtn.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $acceptBtn
+$form.Controls.Add($acceptBtn)
 
 $enableCheckbox = New-Object System.Windows.Forms.CheckBox
 $enableCheckbox.Text = "Enable daily backup"
-$enableCheckbox.Location = New-Object System.Drawing.Point(50,50)
+$enableCheckbox.Location = New-Object System.Drawing.Point(80,40)
 $enableCheckbox.Size = New-Object System.Drawing.Size(200,30)
 $form.Controls.Add($enableCheckbox)
 
 $disableCheckbox = New-Object System.Windows.Forms.CheckBox
 $disableCheckbox.Text = "Disable daily backup"
-$disableCheckbox.Location = New-Object System.Drawing.Point(100,100)
+$disableCheckbox.Location = New-Object System.Drawing.Point(80,70)
 $disableCheckbox.Size = New-Object System.Drawing.Size(200,30)
 $form.Controls.Add($disableCheckbox)
+
+$form.TopMost = $true
+$form.Add_Shown({$textBox.Select()})
+$result = $form.ShowDialog()
