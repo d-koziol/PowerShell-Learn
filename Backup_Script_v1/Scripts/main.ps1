5deb54b8f22ae42d515f55c1d3d7353ae9e2aa2a
Damian Koziol <d.p.koziol@gmail.com>
2025-03-11 11:01:59 +0100

    File organization

---
+++ Backup_Script_v1/Scripts/main.ps1
@@ -0,0 +1,5 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+. "C:\Kopia\PowershellScripts\PowerShell-Learn\Backup_Script_v1\Scripts\logic.ps1"
+. "C:\Kopia\PowershellScripts\PowerShell-Learn\Backup_Script_v1\Scripts\gui.ps1"
